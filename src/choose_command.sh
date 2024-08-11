ff50361c54dc7242bf3867c3f23726bbd1d41b7e
Jaime Sandoval <jaimesandoval@Jaimes-MacBook-Air.local>
2024-08-11 11:38:41 -0700

    Adding cli scripts

---
+++ src/choose_command.sh
@@ -0,0 +1,8 @@
+# echo "# this file is located in 'src/choose_command.sh'"
+# echo "# code for 's3-cli choose' goes here"
+# echo "# you can edit it freely and regenerate (it will not be overwritten)"
+
+export S3_BUCKET_NAME=${args[bucket]}
+
+printf "S3 Bucket set to '$S3_BUCKET_NAME'"
+echo
